eb5f599e1cab19d85de25532f878d2f1b644ec2a
ajaykrishnan <ajayunnikrishnan97@gmail.com>
2024-08-24 16:58:41 +0530

    Made all changes related to responsiveness

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -35,7 +35,7 @@ const Hero = () => {
             animate="visible"
             className='pb-16 text-4xl font-thin tracking-tight lg:mt-4 lg:text-6xl '
           >
-            Ajay Krishnan U.E
+            Ajay Krishnan U.E 
           </motion.h1>
           <motion.span
             variants={container(0.5)}
